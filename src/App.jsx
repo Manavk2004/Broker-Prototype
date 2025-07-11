1682b1809980497d58435a5d2e0e328295a65872
Manavk <mjkamdar04@gmail.com>
2025-07-11 16:27:51 -0600

    Initial order info render using generator function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './App.css'
 
 function App() {
@@ -7,57 +7,72 @@ function App() {
   const [orders, setOrders] = useState([])
   const [currentPrice, setCurrentPrice] = useState("2116.45")
   const [myOrders, setMyOrders] = useState(false)
+  
+
+  //refs
+  const orderCache = useRef([])
+
 
   //functions
 
   function storeOrders(){
-    setOrders((prevOrders) => {
-      return [...prevOrders, {
-        time: new Date().toISOString(),
-        amountPaid: price,
-        pricePerOz: currentPrice,
-        goldSold: price / Number(currentPrice)
-      }]
-    })
+     orderCache.current.push([`${new Date().toISOString()}  amountPaid-${price}  pricePerOz:${currentPrice}, goldSold:${price / Number(currentPrice)} `])
   }
 
-  function formatOrders(orders){ 
-    const workableOrders = JSON.stringify(orders)
-    const parsed = JSON.parse(workableOrders)
-    console.log("Here is the parsed", parsed)
-    const keyValue = Object.entries(parsed[0])
-    console.log("Here are the key values", keyValue)
-    return keyValue.map(([key, value]) => {
-      console.log("Values", value)
-      console.log("Key" , key)
-      if(key === "time"){
-        return(
-          <div class="information-container">
-            <p><strong>{key}:    </strong>{value}</p>
-          </div>
-        )
-      }else if(key === "amountPaid"){
-        return(
-          <div class="information-container">
-            <p><strong>{key}:    </strong>£{value}</p>
-          </div>
-        )
-      }else if(key === "pricePerOz"){
-        return(
-          <div class="information-container">
-            <p><strong>{key}:    </strong>£{value}</p>
-          </div>
-        )
-      }else{
-        return(
-          <div class="information-container">
-            <p><strong>{key}:    </strong>{value} Oz</p>
-          </div>
-        )
-      }
-    })
-
+  function* displayOrders(){
+    for(const order of orderCache.current){
+      yield(
+        <p class="order-info">{order}</p>
+      )
+    }
   }
+
+  const orderElements = [...displayOrders()]
+
+  console.log("The order Elements", orderElements)
+
+
+
+  // function formatOrders(orders){ 
+  //   const index = number
+  //   console.log("This is index", index)
+  //   const workableOrders = JSON.stringify(orders[index])
+  //   console.log('Workable Orders', workableOrders)
+  //   const parsed = JSON.parse(workableOrders)
+  //   console.log("Here is the parsed", parsed)
+  //   const keyValue = Object.entries(parsed)
+  //   // console.log("Here are the key values", keyValue)
+  //   return keyValue.map(([key, value]) => {
+  //     // console.log("Values", value)
+  //     // console.log("Key" , key)
+  //     if(key === "time"){
+  //       return(
+  //         <div class="information-container">
+  //           <p><strong>{key}:    </strong>{value}</p>
+  //         </div>
+  //       )
+  //     }else if(key === "amountPaid"){
+  //       return(
+  //         <div class="information-container">
+  //           <p><strong>{key}:    </strong>£{value}</p>
+  //         </div>
+  //       )
+  //     }else if(key === "pricePerOz"){
+  //       return(
+  //         <div class="information-container">
+  //           <p><strong>{key}:    </strong>£{value}</p>
+  //         </div>
+  //       )
+  //     }else{
+  //       return(
+  //         <div class="information-container">
+  //           <p><strong>{key}:    </strong>{value} Oz</p>
+  //         </div>
+  //       )
+  //     }
+  //   })
+
+  // }
 
 
 
@@ -65,13 +80,17 @@ function App() {
 
 
   //useEffect tests
-  useEffect(()=>{
-    console.log(price)
-  }, [price])
+  // useEffect(()=>{
+  //   console.log(price)
+  // }, [price])
 
-   useEffect(() =>{
-    console.log(orders)
-  })
+  //  useEffect(() =>{
+  //   console.log(orders)
+  // })
+
+  useEffect(()=>{
+    console.log("Here are the orders", orders)
+  }, [orders])
 
 
   return (
@@ -110,11 +129,17 @@ function App() {
       }
       {myOrders && (
         <>
+
           <nav id="order-navBar">
             <a id="nav-button" onClick={() => setMyOrders(false)}>Invest</a>
           </nav>
+
           <h1 id="order-title">Your Orders</h1>
-          <div id="information-page-container">{formatOrders(orders)}</div>
+
+          <div id="information-page-container">
+            {orderElements}
+          </div>
+
         </>
       )}
     </>
