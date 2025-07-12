0fd1f1e9b545c731ffa71b69923ffb6b8192ef39
Manavk <mjkamdar04@gmail.com>
2025-07-12 12:26:02 -0600

    Live price updates with interval cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, useRef } from 'react'
 import './App.css'
+import { livePrices } from "./data/prices.jsx"
 
 function App() {
   //states
@@ -9,6 +10,19 @@ function App() {
   const [myOrders, setMyOrders] = useState(false)
   const [available, setAvailable] = useState(false)
   
+
+  //Direct state changes
+  useEffect( () => {
+      const interval = setInterval(() =>{
+        const randInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+        const index = randInt(0, 19)
+        setCurrentPrice(() =>{
+          return livePrices[index]
+        })
+      }, 3000)
+
+      return () => clearInterval(interval)
+  })
 
   //refs
   const orderCache = useRef([])
@@ -33,68 +47,13 @@ function App() {
 
   const orderElements = [...displayOrders()]
 
-  console.log("The order Elements", orderElements)
+  // console.log("The order Elements", orderElements)
 
 
 
-  // function formatOrders(orders){ 
-  //   const index = number
-  //   console.log("This is index", index)
-  //   const workableOrders = JSON.stringify(orders[index])
-  //   console.log('Workable Orders', workableOrders)
-  //   const parsed = JSON.parse(workableOrders)
-  //   console.log("Here is the parsed", parsed)
-  //   const keyValue = Object.entries(parsed)
-  //   // console.log("Here are the key values", keyValue)
-  //   return keyValue.map(([key, value]) => {
-  //     // console.log("Values", value)
-  //     // console.log("Key" , key)
-  //     if(key === "time"){
-  //       return(
-  //         <div class="information-container">
-  //           <p><strong>{key}:    </strong>{value}</p>
-  //         </div>
-  //       )
-  //     }else if(key === "amountPaid"){
-  //       return(
-  //         <div class="information-container">
-  //           <p><strong>{key}:    </strong>£{value}</p>
-  //         </div>
-  //       )
-  //     }else if(key === "pricePerOz"){
-  //       return(
-  //         <div class="information-container">
-  //           <p><strong>{key}:    </strong>£{value}</p>
-  //         </div>
-  //       )
-  //     }else{
-  //       return(
-  //         <div class="information-container">
-  //           <p><strong>{key}:    </strong>{value} Oz</p>
-  //         </div>
-  //       )
-  //     }
-  //   })
-
-  // }
-
-
-
-
-
-
-  //useEffect tests
   // useEffect(()=>{
-  //   console.log(price)
-  // }, [price])
-
-  //  useEffect(() =>{
-  //   console.log(orders)
-  // })
-
-  useEffect(()=>{
-    console.log("Here are the orders", orders)
-  }, [orders])
+  //   console.log("Here are the orders", orders)
+  // }, [orders])
 
 
   return (
